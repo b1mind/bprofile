3daf03bfb19c54917730dc57add8b3091b9dadf3
b1mind <brent@1mindsolutions.com>
2021-06-21 02:07:40 -0600

    💄 prettier changes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,47 +1,45 @@
 <script>
-	import Header from '$lib/Header/index.svelte';
+  import Header from '$lib/Header/index.svelte'
 
-	// changed up to scss still need to check global for components
-	import '../global.scss';
+  // changed up to scss still need to check global for components
+  import '../global.scss'
 </script>
 
-<Header />
+<Header>
+  <main>
+    <slot />
+  </main>
 
-<main>
-	<slot />
-</main>
+  <footer>
+    <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+  </footer>
 
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
+  <style lang="scss">
+    main {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      padding: 1rem;
+      width: 100%;
+      max-width: 1024px;
+      margin: 0 auto;
+      box-sizing: border-box;
+    }
 
-<style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+    footer {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 40px;
+      @media (min-width: 480px) {
+        padding: 40px 0;
+      }
+    }
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
-	}
-</style>
+    footer a {
+      font-weight: bold;
+    }
+  </style>
+</Header>
+<slot />
