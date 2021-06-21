d3cd7877e7a0c9515bd293ed2d611fb22b519046
b1mind <brent@1mindsolutions.com>
2021-06-21 02:51:23 -0600

    🧙‍♂️ working postcss plugs

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,41 +5,43 @@
   import '../global.scss'
 </script>
 
-<Header>
-  <main>
-    <slot />
-  </main>
+<Header><slot /></Header>
 
-  <footer>
-    <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-  </footer>
+<main>
+  <slot />
+</main>
 
-  <style lang="scss">
-    main {
-      flex: 1;
-      display: flex;
-      flex-direction: column;
-      padding: 1rem;
-      width: 100%;
-      max-width: 1024px;
-      margin: 0 auto;
-      box-sizing: border-box;
+<footer>
+  <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+</footer>
+
+<style lang="scss">
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    width: 100%;
+    max-width: 1024px;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+
+  footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 40px;
+    @media (min-width: 480px) {
+      padding: 40px 0;
     }
+  }
 
-    footer {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      padding: 40px;
-      @media (min-width: 480px) {
-        padding: 40px 0;
-      }
+  footer a {
+    font-weight: bold;
+    @media (min-width: 480px) {
+      font-weight: 400;
     }
-
-    footer a {
-      font-weight: bold;
-    }
-  </style>
-</Header>
-<slot />
+  }
+</style>
