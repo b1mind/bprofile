c546938077d3ebfa5d0ed92007cb33cb3237bdae
b1mind <brent@1mindsolutions.com>
2021-09-04 16:14:37 -0600

    🧪Test: node deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-node'
 import preprocess from 'svelte-preprocess'
 import autoprefixer from 'autoprefixer'
 import postcssCombineMediaQuery from 'postcss-combine-media-query'
@@ -9,7 +9,7 @@ const config = {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
     //defaults for adapter just so I learn them
-    adapter: adapter({ pages: 'build', assets: 'build', fallback: null }),
+    adapter: adapter({}),
   },
   preprocess: [
     preprocess({
